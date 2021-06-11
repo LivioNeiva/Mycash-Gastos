b427a38f815d9b718f3c9df459dedb010243b3a7
Livio Neiva <livio_neiva@hotmail.com>
2021-06-11 02:59:31 -0300

    mycash-PostGres

--- backend/src/main/java/org/mycash/MycashApplication.java
+++ backend/src/main/java/org/mycash/MycashApplication.java
@@ -27,8 +27,8 @@ public class MycashApplication {
 		UsuarioService service = context.getBean(UsuarioService.class);
 
 		//todas as vezes q a aplicação ssubir, vamos criar um usuario
-		service.registraUsuarioAdmin("admin@mycash.com", "admin");
-		service.save("user@mycash.com", "user");
+		//service.registraUsuarioAdmin("admin@mycash.com", "admin");
+		//service.save("user@mycash.com", "user");
 		
 	}
 	
